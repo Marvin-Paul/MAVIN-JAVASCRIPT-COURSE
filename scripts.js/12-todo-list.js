ddc05066582bb542a519755dfb09720e8dc57039
Marvin-Paul <muwerezamarvin2023@gmail.com>
2025-07-24 14:49:04 +0300

    commit update

--- scripts.js/12-todo-list.js
+++ scripts.js/12-todo-list.js
@@ -1,10 +1,38 @@
-const todoList = [];
+ const todoList = [ 'make dinner','wash dishes'];
 
-function addTodo() {
-  const inputElement = document.querySelector('.js-name-input');
-  const todoName = inputElement.value;
-  if (todoName) {
-    todoList.push(todoName);
-    inputElement.value = '';
+  renderTodoList();
+
+
+  function renderTodoList(){
+    let todoListHTML ='';
+
+   for(let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+    const html = 
+    `<p>${todo}
+     <button onclick="
+        todoList.splice(${i},1);
+        renderTodoList();
+     >Delete</button>
+     </p>`;
+    todoListHTML+= html;
+   }
+   console.log(todoListHTML);
+
+     document.querySelector('.js-todo-list')
+      .innerHTML = todoListHTML;
   }
-}
+
+ function addTodo(){
+    const inputElement = document
+    .querySelector('.js-name-input');
+    const name = inputElement.value;
+
+      todoList.push(name);
+      console.log(todoList);
+
+
+      inputElement.value = '';
+      renderTodoList();
+ }
+ 
