8747c65d6b933a1241e960aa7805870ce6787ebe
Marvin-Paul <muwerezamarvin2023@gmail.com>
2025-07-28 21:54:33 +0300

    update todo-list

--- scripts/12-todo-list.js
+++ scripts/12-todo-list.js
@@ -6,7 +6,7 @@ dueDate:'2022-05-23'
  dueDate:'2022-05-24'}
  ];
 
-  renderTodoList();
+ // renderTodoList();
 
 
   function renderTodoList(){
@@ -14,9 +14,8 @@ dueDate:'2022-05-23'
 
    for(let i = 0; i < todoList.length; i++) {
     const todoObject = todoList[i];
-    //const name = todoObject.name;
-    //const dueDate = todoObject.dueDate;
-    const {name, dueDate} = todoObject;
+    const name = todoObject.name;
+    const dueDate = todoObject.dueDate;
     const html = `
     <p>
      <div>${name}</div>
@@ -48,7 +47,7 @@ dueDate:'2022-05-23'
         name : name,
       dueDate:dueDate
  });
-     // console.log(todoList);
+    // console.log(todoList);
 
 
       inputElement.value = '';
